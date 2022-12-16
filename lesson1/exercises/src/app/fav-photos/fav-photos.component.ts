4259c88a4e82df853d7e72a2aa4a43672dda181a
nickimpoco <nickimpoco@gmail.com>
2022-12-16 16:23:11 -0500

    Initial commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Here are the movies I am going to watch:';
+  image1 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.ruthlessreviews.com%2Fwp-content%2Fuploads%2F2019%2F10%2Fparasite-meta.jpg&f=1&nofb=1&ipt=f0578ee62fdf5bf29e26f3b476f2e5e1fb461d8654364e6879f6a5de00cbbc3c&ipo=images';
+  image2 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.theyoungfolks.com%2Fwp-content%2Fuploads%2F2014%2F11%2F0dcfe3c2c4aac937273d5563ca9a029753f1acb6.jpg&f=1&nofb=1&ipt=7aeddfff9f76e90f6ea250b0070dd85e33d1d0e5d96f872d3978fc171254bde7&ipo=images';
+  image3 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F86EJh81aylhncq62OlPnjDCXtTv.jpg&f=1&nofb=1&ipt=70c21f6be99a3b633605f5678bcab860b35712ed41fa8e4f1dd68646760721ee&ipo=images';
 
   constructor() { }
 
